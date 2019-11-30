78178c88c24c837c755dbef8b63e924e6fd116e9
andwis12 <1bandrzejwisinski@gmail.com>
2019-11-30 08:08:50 +0100

    Simple login added still need to work on frontend

--- index.php
+++ index.php
@@ -1,12 +1,66 @@
 <!DOCTYPE HTML>
+<?php
+    session_start();
+
+    ?>
+
 <html lang="pl">
 
 <head>
     <meta charset="utf-8" />
 
+    
+
+    <link rel="stylesheet" href="style/Style.css"/>
+
 </head>
 
+
+
 <body>
+    <div id="main_container">
+        <div id="top">
+            <div id="logo">
+                <img src="logo.png">
+            </div>
+            <div id="search">
+                <input type="text" name="box" placeholder="Czego szukasz?" align="center"/>
+                <button class="search-box-button">&#128269;</button>
+            </div>
+            <div id="login">
+                <div>
+                    <img src="user-profile.png">
+                </div>
+                    <div>
+                        <?php
+                            if(!isset($_SESSION['user']))
+                            {
+                                echo "Zaloguj się \n Załóż konto";
+                                header('login.php');
+                            }else
+                            {
+                                echo "Moje konto \n Wyloguj";
+                            }
+                        ?>
+                    </div >
+            </div>
+            <div class="items">
+
+            </div>
+        </div>
+        <div id="menu">
+            <nav>
+                <ul>
+                    <li><a href="">Laptopy i tablety</a></li>
+                    <li><a href="">Telefony i GPS</a></li>
+                    <li><a href="">Komputery stacjonarne</a></li>
+                    <li><a href="">Podzespoły komputerowe</a></li>
+                    <li><a href="">Akcesoria</a></li>
+                </ul>
+            </nav>
+
+        </div>
+    </div>
 
 <body>
 </html>
--- login.php
+++ login.php
@@ -4,13 +4,33 @@
 <head>
     <meta charset="utf-8" />
 
+    <link rel="stylesheet" href="style/loginStyle.css"/>
+
 </head>
 
 <body>
+    <form class="login-form" action="zaloguj.php" method="post">
+        <h1>Login</h1>
+        <div class="txtb">
+            <input type="text" placeholder="Username" name="login">
+        </div>
 
-<?php
-    
-?>
+        <div class="txtb">
+            <input type="password" placeholder="Password" name="password">
+
+        </div>
+
+        <input type="submit" class="logbtn" value="Login">
+
+
+        <div class="bottom-text">
+            Don't have account sign up here
+        </div>
+
+
+    </form>
+
+
 
 <body>
 </html>
